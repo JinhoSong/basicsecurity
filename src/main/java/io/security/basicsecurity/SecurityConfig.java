21a1fe9a09c1161ed6e560c0baa53c1349a91b55
JinhoSong <chosunz4@naver.com>
2020-10-22 14:33:07 +0900

    remember me 실습

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,10 +1,12 @@
 package io.security.basicsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -18,6 +20,9 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -44,8 +49,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         response.sendRedirect("/login");
                     }
                 })
-                .deleteCookies("remember-me"); // 서버에서 만든 쿠키를 삭제하고 싶으면 해당 이름을 넣으면 된다.
-
+            .and()
+                .rememberMe()// 기능 사용을 명시
+                .rememberMeParameter("remeber") // 파라미터 이름 적용
+                .tokenValiditySeconds(3600) // 초 단위로 설정
+                .userDetailsService(userDetailsService);
 
     }
 }
